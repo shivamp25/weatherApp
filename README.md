2ed2585c9957a3bb52f76a33f279983063dc3875
Shivam <shivampadaliya25@gmail.com>
2019-01-17 21:25:42 -0500

    weatherApp

---
+++ README.md
@@ -0,0 +1,7 @@
+### Getting Started
+```
+> git clone https://github.com/shivamp25/weatherApp.git
+> cd weatherApp
+> npm install
+> npm start
+```
---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const API_KEY = '572656b9c57f4c026a2c41edcd806008';
+const API_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${API_URL}&q=${city},us`;
+	const request = axios.get(url);
+	
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
